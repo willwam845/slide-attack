c4627f082b8d26e4900a432eebdb4eed9c44d018
willwam <willwam845@gmail.com>
2025-09-04 20:32:41 +0100

    recover slid pairs

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,16 @@ type Sbox = [u8; SBOX_SIZE];
 
 type BlockData = BitArray<[u8; BLOCK_SIZE]>;
 type KeyData = BitArray<[u8; KEY_SIZE]>;
+type SubkeyData = BitArray<[u8; BLOCK_SIZE]>;
 
 #[derive(Debug, Clone, Copy)]
 struct Block(BlockData);
 
 #[derive(Debug, Clone, Copy)]
 struct Key(KeyData);
-type Subkey = BlockData;
+
+#[derive(Debug, Clone, Copy)]
+struct Subkey(SubkeyData);
 
 struct Cipher {
     sbox: Sbox,
@@ -33,38 +36,18 @@ impl Cipher {
         Cipher { sbox, subkeys }
     }
 
-    fn encrypt_block(&mut self, block: &mut Block) {
+    fn encrypt_block(&mut self, block: Block) -> Block {
+        let mut block = block;
         for round_num in 0..ROUNDS {
-            self.encrypt_round(block, self.subkeys[(round_num as usize) % self.subkeys.len()]);
+            block = self.encrypt_round(block, self.subkeys[(round_num as usize) % self.subkeys.len()]);
         }
+        block
     }
 
-    fn encrypt_round(&mut self, block: &mut Block, subkey: Subkey) {
-        self.add_subkey(block, subkey);
-        self.sub_bytes(block);
+    fn encrypt_round(&mut self, block: Block, subkey: Subkey) -> Block {
+        let block = block.add_subkey(subkey);
+        block.sub_bytes(self.sbox)
     }
-
-    fn add_subkey(&mut self, block: &mut Block, subkey: Subkey) {
-        for (mut x1, x2) in block.get_data_mut().iter_mut().zip(subkey.iter()) {
-            *x1 ^= *x2
-        }
-    }
-
-    fn sub_bytes(&mut self, block: &mut Block) {
-        for chunk in block.get_data_mut().chunks_mut(SBOX_BITS) {
-            let nibble = bits_to_byte(chunk.iter().by_vals());
-            let substituted = self.sbox[nibble as usize];
-            for i in 0..SBOX_BITS {
-                chunk.set(i as usize, (substituted >> i & 1) != 0)
-            }
-        }
-    }
-}
-
-fn bits_to_byte<I: Iterator<Item = bool>>(bits: I) -> u8 {
-    bits.enumerate()
-        .map(|(i, bit)| if bit { 1 << i } else { 0 })
-        .sum()
 }
 
 impl Block {
@@ -75,12 +58,27 @@ impl Block {
         }
         Block(block_data)
     }
+    fn to_byte(&self) -> u8 { bits_to_byte(self.get_data().as_bitslice().iter().by_vals()) }
 
     fn get_data(&self) -> &BlockData { &self.0 }
     fn get_data_mut(&mut self) -> &mut BlockData { &mut self.0 }
 
-    fn to_byte(&self) -> u8 {
-        bits_to_byte(self.get_data().as_bitslice().iter().by_vals())
+    fn add_subkey(mut self, subkey: Subkey) -> Block {
+        for (mut x1, x2) in self.get_data_mut().iter_mut().zip(subkey.get_data().iter()) {
+            *x1 ^= *x2
+        }
+        self
+    }
+
+    fn sub_bytes(mut self, sbox: Sbox) -> Block {
+        for chunk in self.get_data_mut().chunks_mut(SBOX_BITS) {
+            let nibble = bits_to_byte(chunk.iter().by_vals());
+            let substituted = sbox[nibble as usize];
+            for i in 0..SBOX_BITS {
+                chunk.set(i as usize, (substituted >> i & 1) != 0)
+            }
+        }
+        self
     }
 }
 
@@ -95,14 +93,90 @@ impl Key {
     fn split_to_keys(&self) -> Vec<Subkey> {
         let mut subkeys: Vec<Subkey> = Vec::new();
         for chunk in self.get_data().chunks(BLOCK_BITS) {
-            let mut subkey: Subkey = Subkey::ZERO;
+            let mut subkey_data: SubkeyData = SubkeyData::ZERO;
             for (i, bit) in chunk.iter().by_vals().enumerate() {
-                subkey.set(i, bit);
+                subkey_data.set(i, bit);
             }
-            subkeys.push(subkey);
+            subkeys.push(Subkey(subkey_data));
         }
         subkeys
     }
+}
+
+impl Subkey {
+    fn from_byte(byte: u8) -> Subkey {
+        let mut subkey_data = SubkeyData::ZERO;
+        for i in 0..8 {
+            subkey_data.set(i as usize, (byte >> i & 1) != 0)
+        }
+        Subkey(subkey_data)
+    }
+    fn to_byte(&self) -> u8 { bits_to_byte(self.get_data().as_bitslice().iter().by_vals()) }
+
+    fn get_data(&self) -> &SubkeyData { &self.0 }
+    fn get_data_mut(&mut self) -> &mut SubkeyData { &mut self.0 }
+}
+
+fn bits_to_byte<I: Iterator<Item = bool>>(bits: I) -> u8 {
+    bits.enumerate()
+        .map(|(i, bit)| if bit { 1 << i } else { 0 })
+        .sum()
+}
+
+fn generate_inverse_sbox(sbox: Sbox) -> Sbox {
+    (0..SBOX_SIZE)
+        .map(|i| sbox.iter().position(|&x| x == i as u8).unwrap() as u8)
+        .collect::<Vec<u8>>()
+        .try_into()
+        .unwrap()
+}
+
+fn recover_key_combinations(pt1: Block, pt2: Block, sbox: Sbox) -> Vec<(Subkey, Subkey)> {
+    let mut key_combinations = Vec::new();
+    let inv_sbox: Sbox = generate_inverse_sbox(sbox);
+    pt2.sub_bytes(inv_sbox);
+
+    for k0_byte in 0..1<<BLOCK_SIZE {
+        let k0: Subkey = Subkey::from_byte(k0_byte);
+        let pt = pt1.add_subkey(k0).sub_bytes(sbox);
+        let k1 = Subkey::from_byte(pt.to_byte() ^ pt2.to_byte());
+        key_combinations.push((k0, k1))
+    };
+
+    key_combinations
+}
+
+fn check_slid_pair(pt1: Block, pt2: Block, ct1: Block, ct2: Block, sbox: Sbox) -> bool {
+    let key_guesses = recover_key_combinations(pt1, pt2, sbox);
+    for (k0, k1) in key_guesses.iter() {
+        let ct = ct1.add_subkey(*k0)
+            .sub_bytes(sbox)
+            .add_subkey(*k1)
+            .sub_bytes(sbox);
+
+        if ct.to_byte() == ct2.to_byte() {
+            return true
+        }
+    }
+    false
+}
+
+fn find_slid_pairs(plaintexts: &Vec<Block>, ciphertexts: &Vec<Block>, sbox: Sbox) -> Vec<(u8, u8)> {
+    let mut slid_pairs = Vec::new();
+    for i in 0..plaintexts.len() {
+        let pt1 = plaintexts[i];
+        let ct1 = ciphertexts[i];
+        for j in 0..plaintexts.len() {
+            let pt2 = plaintexts[j];
+            let ct2 = ciphertexts[j];
+            if i != j {
+                if check_slid_pair(pt1, pt2, ct1, ct2, sbox) {
+                    slid_pairs.push((i as u8, j as u8));
+                }
+            }
+        }
+    }
+    slid_pairs
 }
 
 fn main() {
@@ -120,12 +194,16 @@ fn main() {
     let mut plaintexts: Vec<Block> = Vec::new();
     let mut ciphertexts: Vec<Block> = Vec::new();
 
-    for _ in 0..256 {
+    for _ in 0..30 {
         let plaintext: Block = Block::from_byte(rng.next_u32() as u8);
-        let mut ciphertext = plaintext.clone();
-        cip.encrypt_block(&mut ciphertext);
+        let ciphertext = cip.encrypt_block(plaintext);
         plaintexts.push(plaintext);
         ciphertexts.push(ciphertext);
+    }
+
+    let slid_pairs = find_slid_pairs(&plaintexts, &ciphertexts, sbox);
+    for (pt1, pt2) in slid_pairs {
+        println!("{} {}", pt1, pt2);
     }
 
     println!("{:?}", plaintexts.iter().map(|block| block.to_byte()).collect::<Vec<u8>>());
